3d648507c2d1b1b8cf77c4e9c2f3cbacb5af5593
Genius-Excel <geniusexcel9@gmail.com>
2024-07-25 19:55:16 +0100

    Test cases with chai.

---
+++ 0x06-unittests_in_js/2-calcul.test.js
@@ -0,0 +1,67 @@
+const chai = require('chai');
+const assert = chai.assert;
+const calculateNumber = require('./2-calcul')
+
+
+describe('calculateNumber', () => {
+    it('...', () => {
+        assert.strictEqual(calculateNumber('SUM', 2.0, 5.4), 7);
+    });
+
+    it('...', () => {
+        assert.strictEqual(calculateNumber('SUBTRACT', 2, 10), 8);
+    });
+
+    it('...', () => {
+        assert.strictEqual(calculateNumber('DIVIDE', 9.5, 5), 2);
+    });
+
+    it('...', () => {
+        assert.strictEqual(calculateNumber('SUM', 10.5, 5), 16);
+    });
+
+    it('...', () => {
+        assert.strictEqual(calculateNumber('SUBTRACT', 2, 5), 3);
+    });
+
+    it('...', () => {
+        assert.strictEqual(calculateNumber('DIVIDE', 4.3, 2.4), 2);
+    });
+
+    it('...', () => {
+        assert.strictEqual(calculateNumber('SUM', 3.5, 3.5), 8);
+    });
+
+    it('...', () => {
+        assert.strictEqual(calculateNumber('SUBTRACT', 6.2, 3.4), -3);
+    });
+
+    it('...', () => {
+        assert.strictEqual(calculateNumber('DIVIDE', 2, 0), "Error");
+    });
+
+    it('...', () => {
+        assert.strictEqual(calculateNumber('SUM', 6.2, 3.4), 9);
+    });
+
+    it('...', () => {
+        assert.strictEqual(calculateNumber('SUBTRACT', 8.5, 3.4), -6);
+    });
+    
+    it('...', () => {
+        assert.strictEqual(calculateNumber('DIVIDE', 0, 2.5), 0);
+    });
+
+    it('...', () => {
+        assert.strictEqual(calculateNumber('SUM', 8.2, 3.4), 11);
+    });
+
+    it('...', () => {
+        assert.strictEqual(calculateNumber('SUBTRACT', 10, 3), -7);
+    });
+    
+    it('...', () => {
+        assert.strictEqual(calculateNumber('DIVIDE', 15, 2.5), 5);
+    });
+
+});
