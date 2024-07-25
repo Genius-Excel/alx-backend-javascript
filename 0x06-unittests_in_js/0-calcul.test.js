564bf2bcb691305413fae2075acef6d5128ad247
Genius-Excel <geniusexcel9@gmail.com>
2024-07-25 19:22:32 +0100

    New test cases added.

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -13,4 +13,24 @@ describe('calculateNumber', () => {
     it('...', () => {
         assert.strictEqual(calculateNumber(2.5, 4), 7);
     });
-})
+
+    it('...', () => {
+        assert.strictEqual(calculateNumber(1.5, 1.4), 3);
+      });
+    
+    it('...', () => {
+        assert.strictEqual(calculateNumber(-4, -2), -6);
+    });
+    
+    it('...', () => {
+        assert.strictEqual(calculateNumber(2.5, 1.5), 5);
+    });
+
+    it('...', () => {
+        assert.strictEqual(calculateNumber(0.5, 1.5), 3);
+    });
+
+    it('...', () => {
+        assert.strictEqual(calculateNumber(2.4, 0.5), 3);
+    });
+});
