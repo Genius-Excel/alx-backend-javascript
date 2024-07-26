6f329d4dc40b98b7ab668d9d6a2bcd6da3a0bfc4
Genius-Excel <geniusexcel9@gmail.com>
2024-07-26 19:25:05 +0100

    Test file renamed.

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,22 @@
+const expect = require('chai').expect;
+const getPaymentTokenFromApi = require('./6-token_payment')
+
+
+describe('getPaymentTokenFromApi', (done) => {
+    it('...', () => {
+        getPaymentTokenFromApi(true).then((response) => {
+            expect(response).to.deep.equal({'data': 'Successful response from the API'});
+            done();
+        }).catch((error) => {
+            done(error);
+        });
+
+    });
+
+    it('...', (done) => {
+        const result = getPaymentTokenFromApi(false);
+        expect(result).to.be.undefined;
+        done();
+    });
+
+});
