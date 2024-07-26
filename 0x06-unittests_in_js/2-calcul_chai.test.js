0613859e888d454718f4fa60ef7745dc98d12e17
Genius-Excel <geniusexcel9@gmail.com>
2024-07-26 17:05:36 +0100

    Test cases modified to use 'Chai' library 'expect'.

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,67 +1,67 @@
 const chai = require('chai');
-const assert = chai.assert;
+const expect = chai.expect;
 const calculateNumber = require('./2-calcul_chai')
 
 
 describe('calculateNumber', () => {
     it('...', () => {
-        assert.strictEqual(calculateNumber('SUM', 2.0, 5.4), 7);
+        expect(calculateNumber('SUM', 2.0, 5.4)).to.equal(7);
     });
 
     it('...', () => {
-        assert.strictEqual(calculateNumber('SUBTRACT', 2, 10), 8);
+        expect(calculateNumber('SUBTRACT', 2, 10)).to.equal(8);
     });
 
     it('...', () => {
-        assert.strictEqual(calculateNumber('DIVIDE', 9.5, 5), 2);
+        expect(calculateNumber('DIVIDE', 9.5, 5)).to.equal(2);
     });
 
     it('...', () => {
-        assert.strictEqual(calculateNumber('SUM', 10.5, 5), 16);
+        expect(calculateNumber('SUM', 10.5, 5)).to.equal(16);
     });
 
     it('...', () => {
-        assert.strictEqual(calculateNumber('SUBTRACT', 2, 5), 3);
+        expect(calculateNumber('SUBTRACT', 2, 5)).to.equal(3);
     });
 
     it('...', () => {
-        assert.strictEqual(calculateNumber('DIVIDE', 4.3, 2.4), 2);
+        expect(calculateNumber('DIVIDE', 4.3, 2.4)).to.equal(2);
     });
 
     it('...', () => {
-        assert.strictEqual(calculateNumber('SUM', 3.5, 3.5), 8);
+        expect(calculateNumber('SUM', 3.5, 3.5)).to.equal(8);
     });
 
     it('...', () => {
-        assert.strictEqual(calculateNumber('SUBTRACT', 6.2, 3.4), -3);
+        expect(calculateNumber('SUBTRACT', 6.2, 3.4)).to.equal(-3);
     });
 
     it('...', () => {
-        assert.strictEqual(calculateNumber('DIVIDE', 2, 0), "Error");
+        expect(calculateNumber('DIVIDE', 2, 0)).to.equal("Error");
     });
 
     it('...', () => {
-        assert.strictEqual(calculateNumber('SUM', 6.2, 3.4), 9);
+        expect(calculateNumber('SUM', 6.2, 3.4)).to.equal(9);
     });
 
     it('...', () => {
-        assert.strictEqual(calculateNumber('SUBTRACT', 8.5, 3.4), -6);
+        expect(calculateNumber('SUBTRACT', 8.5, 3.4)).to.equal(-6);
     });
     
     it('...', () => {
-        assert.strictEqual(calculateNumber('DIVIDE', 0, 2.5), 0);
+        expect(calculateNumber('DIVIDE', 0, 2.5)).to.equal(0);
     });
 
     it('...', () => {
-        assert.strictEqual(calculateNumber('SUM', 8.2, 3.4), 11);
+        expect(calculateNumber('SUM', 8.2, 3.4)).to.equal(11);
     });
 
     it('...', () => {
-        assert.strictEqual(calculateNumber('SUBTRACT', 10, 3), -7);
+        expect(calculateNumber('SUBTRACT', 10, 3)).to.equal(-7);
     });
     
     it('...', () => {
-        assert.strictEqual(calculateNumber('DIVIDE', 15, 2.5), 5);
+        expect(calculateNumber('DIVIDE', 15, 2.5)).to.equal(5);
     });
 
 });
