6914bccb0aa22efe12ba7b766bf14f3f9dd6f32c
Genius-Excel <geniusexcel9@gmail.com>
2024-07-26 19:20:25 +0100

    This module contains a function that an API response.

---
+++ 0x06-unittests_in_js/6-token_payment.js
@@ -0,0 +1,8 @@
+function getPaymentTokenFromApi(success) {
+    if (success) {
+        return Promise.resolve({'data': 'Successful response from the API'});
+    }
+}
+
+
+module.exports = getPaymentTokenFromApi;
